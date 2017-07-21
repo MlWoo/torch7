86a73d77af9b197a77790ea8afc5057ed1040d50
MlWoo <menglin.wu@intel.com>
2017-07-21 10:56:33 +0800

    copy and fill other type(except float and double) paral opt

--- lib/TH/generic/THVectorDefault.c
+++ lib/TH/generic/THVectorDefault.c
@@ -5,31 +5,29 @@
 void THVector_(copy_DEFAULT)(real *x, const real *y, const ptrdiff_t n) {
   ptrdiff_t i = 0;
 
-  for(; i <n-4; i+=4)
+#ifdef _OPENMP
+  int omp_flag = omp_in_parallel();
+  #pragma omp parallel for if ( (n > TH_OMP_OVERHEAD_THRESHOLD_VEC) && ( 0 == omp_flag) ) private (i)
+#endif
+  #pragma ivdep
+  for(i=0; i <n; i++)
   {
     x[i] = y[i];
-    x[i+1] = y[i+1];
-    x[i+2] = y[i+2];
-    x[i+3] = y[i+3];
   }
-
-  for(; i < n; i++)
-    x[i] = y[i];
 }
 
 void THVector_(fill_DEFAULT)(real *x, const real c, const ptrdiff_t n) {
   ptrdiff_t i = 0;
 
-  for(; i <n-4; i+=4)
+#ifdef _OPENMP
+  int omp_flag = omp_in_parallel();
+  #pragma omp parallel for if ( (n > TH_OMP_OVERHEAD_THRESHOLD_VEC) && ( 0 == omp_flag) ) private (i)
+#endif
+  #pragma ivdep
+  for(i=0; i <n; i++)
   {
     x[i] = c;
-    x[i+1] = c;
-    x[i+2] = c;
-    x[i+3] = c;
   }
-
-  for(; i < n; i++)
-    x[i] = c;
 }
 
 void THVector_(cadd_DEFAULT)(real *z, const real *x, const real *y, const real c, const ptrdiff_t n)
