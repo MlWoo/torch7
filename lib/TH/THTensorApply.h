5ce902eec040857c4c6c87b58852fd02ae628a34
MlWoo <megnlin.wu@intel.com>
2017-08-02 10:47:14 +0800

    gcc and icc on skylake

--- lib/TH/THTensorApply.h
+++ lib/TH/THTensorApply.h
@@ -181,7 +181,7 @@
 #define THTENSOR_MAX_DIM 64
 #define TH_OMP_OVERHEAD_THRESHOLD_COPY 1000 
 #include <x86intrin.h>
-#include "general.h"
+#include "generic/general.h"
 
 #define TH_TENSOR_APPLY_REDUCTION_ADVANCED_INDEX(TYPE1, TENSOR1, OPERATION, CODE) \
 {                                                                               \
--- lib/TH/generic/general.h
+++ lib/TH/generic/general.h
@@ -1,21 +1,5 @@
 #ifndef GENERAL_H
 #define GENERAL_H
-typedef unsigned long RNG;
-
-inline RNG RNGInit(long seed)
-{
-    RNG rng = seed ? (unsigned long)seed : (unsigned long)(long)-1;
-    return rng;
-}
-
-inline unsigned int RandInt( RNG* rng )
-{
-    unsigned long temp = *rng;
-    temp = (unsigned long)(unsigned)temp*1554115554 + (temp >> 32);
-    *rng = temp;
-    return (unsigned long)temp;
-}
-
 inline ptrdiff_t SearchingIndex(ptrdiff_t index, long *stride, long dim, long* size)
 {
   ptrdiff_t i = 0;
